380e626ec9fd05ef395c216de5828e7cbbef33c3
ech1 <ech1@nowhere.com>
2020-07-09 17:04:55 +0200

    webdavconf

--- keepass/webdav.sh
+++ keepass/webdav.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install nginx-full
-sudo wget https://raw.githubusercontent.com/ech1/serverside/keepass/nginx.conf -O /etc/nginx/nginx.conf
+sudo wget https://raw.githubusercontent.com/ech1/serverside/master/keepass/nginx.conf -O /etc/nginx/nginx.conf
+sudo wget https://raw.githubusercontent.com/ech1/serverside/master/keepass/0-webdav.conf -O /etc/nginx/conf.d/0-webdav.conf
