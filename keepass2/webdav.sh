91b14faa8eee14788230fbad3b67709c08d06ade
ech1 <ech1@nowhere.com>
2020-07-11 21:19:26 +0200

    keepasswebdav

---
+++ keepass2/webdav.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 apache2-utils curl wget -y
+
+sudo a2enmod dav
+sudo a2enmod dav_fs
+
+sudo mkdir /webdav
+sudo mkdir /var/www/webdav
+sudo chown -R www-data:www-data /var/www/
+
+#then we have to edit the main virtual host config with root priv
+#sudo nano /etc/apache2/sites-avaialable/000-default.conf
+cd /etc/apache2/sites-available
+sudo curl https://raw.githubusercontent.com/ech1/serverside/master/keepass2/000-default.conf
+
+sudo htdigest -c /etc/apache2/users.password webdav $USER
+sudo chown www-data:www-data /etc/apache2/users.password
+
+sudo a2enmod auth_digest
+sudo service apache2 restart
+
+sudo ip a | grep inet
+#the url is http://<IP>/webdav
+#or dav://<IP>/webdav
