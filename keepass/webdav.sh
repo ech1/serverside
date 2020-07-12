99fe2e28ded1df3624e88fad68ca9c2b50161834
ech1 <ech1@nowhere.com>
2020-07-12 03:01:13 +0200

    keepass wont work reee

--- keepass/webdav.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install nginx-full
-sudo wget https://raw.githubusercontent.com/ech1/serverside/master/keepass/nginx.conf -O /etc/nginx/nginx.conf
-sudo wget https://raw.githubusercontent.com/ech1/serverside/master/keepass/0-webdav.conf -O /etc/nginx/conf.d/0-webdav.conf
-echo -n "$USER:" | sudo tee /etc/nginx/.passwords.list
-openssl passwd -apr1 | sudo tee -a /etc/nginx/.passwords.list
-sudo nginx -t
-sudo systemctl restart nginx
