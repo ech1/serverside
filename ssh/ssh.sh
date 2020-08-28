eb02707fba892597ac8754114dfd0c52d0ff59d9
ech1 <ech1@nowhere.com>
2020-08-28 11:22:57 +0100

    ssh

--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -4,11 +4,19 @@ sudo apt install openssh-server
 sudo systemctl status ssh
 cd /etc/ssh
 sudo rm sshd_config
-sudo wget 
+sudo wget https://raw.githubusercontent.com/ech1/serverside/master/ssh/sshd_config
+sudo systemctl reload ssh
 
+#WE ARE ON THE SERVER !!!
+#So we generate the public ssh key
+ssh-keygen -t ed25519
+cd ~/.ssh/
+cat id_ed25519.pub >> authorized_keys
 
-
-
+#ssh server has the public key
+#we give the private key to the user somehow
+#the user puts id_ed25519 into his own ~/.ssh/
+#and only after he can login
 
 
 
