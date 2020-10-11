65bdecdf5cbffaf408b3822429a58eedc35725f3
ech1 <ech1@nowhere.com>
2020-10-11 12:30:56 +0200

    stuff

---
+++ ssh/ssh_arch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo pacman -Syu
+sudo pacman -S openssh
+cd /etc/ssh
+sudo rm sshd_config
+sudo wget https://raw.githubusercontent.com/ech1/serverside/master/ssh/sshd_config
+sudo systemctl enable sshd --now
+sudo systemctl status sshd
+
+mkdir ~/.ssh/
+cd ~/.ssh/
+
+ssh-keygen -t ed25519
+cat id_ed25519.pub >> authorized_keys
+
+
